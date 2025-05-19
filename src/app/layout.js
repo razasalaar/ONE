4c73ffca4b19395d371d84d37cccbf65e86cd73a
Raza Fazal <razafazal19@gmail.com>
2025-05-19 16:05:36 +0500

    accepted

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,21 @@ import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
+
 const queryClient = new QueryClient();
+
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
   return (
     <Provider store={store}>
       <html lang="en">
         <body>
           <QueryClientProvider client={queryClient}>
-            {children}
+            <div className="page-transition" key={pathname}>
+              {children}
+            </div>
           </QueryClientProvider>
         </body>
       </html>
